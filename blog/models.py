6b815f65a80dc7662f853883b0838bd6da8db324
sloan <1172530484@qq.com>
2019-02-14 15:32:14 +0800

    博客数据建模，并实现在models中

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Category(models.Model):
+    """
+    文章分类
+    """
+    name = models.CharField(verbose_name='文章类别', max_length=20)
+
+    class Meta:
+        verbose_name = '文章类别'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    """
+    文章标签
+    """
+    name = models.CharField(verbose_name='文章标签', max_length=20)
+
+    class Meta:
+        verbose_name = '文章标签'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    """
+    博客
+    """
+    title = models.CharField(verbose_name='标题', max_length=100)
+    # 文章摘要，可以没有文章摘要，但默认情况下 CharField 要求我们必须存入数据，否则就会报错。
+    # 指定 CharField 的 blank=True 参数值后就可以允许空值了。
+    excerpt = models.CharField(verbose_name='摘要',max_length=200, blank=True,default='')
+    # 存储比较短的字符串可以使用 CharField，但对于文章的正文来说可能会是一大段文本，因此使用 TextField 来存储大段文本。
+    content = models.TextField(verbose_name='正文', default='')
+    create_time = models.DateTimeField(verbose_name='创建时间', default=timezone.now())
+    modify_time = models.DateTimeField(verbose_name='修改时间', auto_now=True)
+    click_nums = models.IntegerField(verbose_name='点击量')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='文章类别')
+    # blank = True 参数值后就可以允许空值了。
+    tag = models.ManyToManyField(Tag, blank=True, verbose_name='文章标签')
+    # 文章作者，这里 User 是从 django.contrib.auth.models 导入的。
+    # django.contrib.auth 是 Django 内置的应用，专门用于处理网站用户的注册、登录等流程，User 是 Django 为我们已经写好的用户模型。
+    # 这里我们通过 ForeignKey 把文章和 User 关联了起来。
+    # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和 Category 类似。
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
+
+
+    class Meta:
+        verbose_name = '我的博客'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
