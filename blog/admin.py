154e0ea03f25620e33b2ca77533880f16474bd18
sloan <1172530484@qq.com>
2019-02-14 16:21:29 +0800

    1.admin后台管理，应用显示的注册 2.点击进入对应的博客表，显示的列以及显示列的字段顺序

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import Blog, Category, Tag
+
+# 后台管理 显示列的设置
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title','excerpt', 'click_nums', 'category', 'create_time', 'modify_time', 'author']
+
+#admin 后台的注册
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
