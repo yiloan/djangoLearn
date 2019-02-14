61b08a5a2cc62b2285a4cf9e15b5b21dba2f538b
sloan <1172530484@qq.com>
2019-02-14 09:59:09 +0800

    设置mysql数据库连接

--- djangoLearn/settings.py
+++ djangoLearn/settings.py
@@ -79,9 +79,18 @@ WSGI_APPLICATION = 'djangoLearn.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',        #数据库名
+        'USER':'root',           #用户名
+        'PASSWORD':'mysql',     #密码
+        'HOST':'127.0.0.1',      #本机地址
+        'PORT':'3306',           #端口
+
     }
 }
 
