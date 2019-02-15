7abca55ad159f7bcab956db4b3010653cdefbaa3
sloan <1172530484@qq.com>
2019-02-15 14:29:33 +0800

    index.html改造 使用模板继承，并对模板不同的部分进行标志

--- blog/models.py
+++ blog/models.py
@@ -39,12 +39,12 @@ class Blog(models.Model):
     title = models.CharField(verbose_name='标题', max_length=100)
     # 文章摘要，可以没有文章摘要，但默认情况下 CharField 要求我们必须存入数据，否则就会报错。
     # 指定 CharField 的 blank=True 参数值后就可以允许空值了。
-    excerpt = models.CharField(verbose_name='摘要',max_length=200, blank=True,default='')
+    excerpt = models.CharField(verbose_name='摘要', max_length=200, blank=True, default='')
     # 存储比较短的字符串可以使用 CharField，但对于文章的正文来说可能会是一大段文本，因此使用 TextField 来存储大段文本。
     content = models.TextField(verbose_name='正文', default='')
     create_time = models.DateTimeField(verbose_name='创建时间', default=timezone.now())
     modify_time = models.DateTimeField(verbose_name='修改时间', auto_now=True)
-    click_nums = models.IntegerField(verbose_name='点击量')
+    click_nums = models.IntegerField(verbose_name='点击量',default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='文章类别')
     # blank = True 参数值后就可以允许空值了。
     tag = models.ManyToManyField(Tag, blank=True, verbose_name='文章标签')
@@ -53,7 +53,6 @@ class Blog(models.Model):
     # 这里我们通过 ForeignKey 把文章和 User 关联了起来。
     # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和 Category 类似。
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
-
 
     class Meta:
         verbose_name = '我的博客'
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+from blog.models import Blog,Category,Tag
+
 
 # Create your views here.
+
+class IndexView(View):
+    """
+        首页
+    """
+
+    def get(self, request):
+        all_blog = Blog.objects.all().order_by('-id')
+        return render(request, 'index.html', {'all_blog': all_blog})
